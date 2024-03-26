1929e3d4e89eef3b82840a05308692536bce9436
Joan Alejandro Dos Santos Kladnik <154430250+joankl@users.noreply.github.com>
2024-03-26 12:21:08 +0000

    Update of Analysis_simulation
    USADO EN CENTER POINT GEN - adaptación a código sin partición de jobs

--- analysis_simulation.py
+++ analysis_simulation.py
@@ -11,6 +11,7 @@ from numpy import array, where, shape, reshape
 
 import pandas as pd
 
+import itertools
 #Useful functions for calculations and plots--------------------------------------------------
 
 def magnitude(vector): 
@@ -44,17 +45,17 @@ def Analysis_simulation(file, evID):
 	"""
 	Parametros:
 	:file -> type(file) = uproot.reading.ReadOnlyDirectory
-	:evID -> types(file) = list
+	:evID -> type(evID) = list
 	
 	Funtion which recive the simu. file, and eventID and analize each ID in a given subset of simulation
 
 	"""
 
 	# Simulation Info
-	data1 = file['T;7']
+	data1 = file['T;1']
 
 	#Extract Simulation Data
-	evtid = np.array(data1['evtid'])
+	mcID = np.array(data1['mcID'])
 	mc_position = np.array(data1['mc_position'])
 	position = np.array(data1['position'])
 	hit_pmtid = np.array(data1['hit_pmtid'])
@@ -82,40 +83,33 @@ def Analysis_simulation(file, evID):
 	hit_pmtid_ID = []									# ID of PMTs giving hits
 	hit_type_ID = []									# hit_type of the eventID
 	mc_postion_vertex = []								# coordinates of vertex generation
+	time_res_ID = []									# (cleaned) time residual of the event ID
+	pmt_xyz_ID = []										# pmt xyz coordintas for every ID
+	pmt_sph_ID = []										# pmt sph coordintas for every ID
+
+	#hit_type separation
+	pmt_xyz_hit_1_ID = []
+	pmt_xyz_hit_2_ID = []
+
+	pmt_sph_hit_1_ID = []
+	pmt_sph_hit_2_ID = []
+
+	time_residual_hit1_ID = []							
+	time_residual_hit2_ID = []							
+
+	hitpmt_hit_1_ID = []							
+	hitpmt_hit_2_ID = []
+
+
+	pmt_ID_control = []
 
 	for i_id in evID:
 
-		#When simulation data includes many simulations the IDs are repeated, then execute the following to separate subsets:
-		condition_1 = (evtid == i_id)
-		index = where(condition_1)[0]               
-		break_data = np.array([index[0]], dtype = np.int64)    #indices where data Breaks - Indices where subsets start
-		# extract the index where data breaks
-		for i in range(len(index)-1):
-		    if index[i+1] != index[i]+1:
-		    	break_data = np.append(break_data,index[i+1])
-		        
-		break_data = np.append(break_data,index[-1]+1)
-
-		subset = [0,1]  #first subset
-
-		init =  break_data[subset[0]]
-		final_ = break_data[subset[1]]
-
-		#Extract new data of subset: 
-		sub_evtid = evtid[init:final_]
-		sub_mc_position = mc_position[init:final_]
-		sub_position = position[init:final_]
-		sub_hit_pmtid = hit_pmtid[init:final_]
-		sub_hit_pmttime = hit_pmttime[init:final_]
-		sub_hit_residual = hit_residual[init:final_]
-		sub_hit_type = hit_type[init:final_]
-
-		#Retaking desired data of eventID  in subset
-		condition_2 = (sub_evtid == i_id)
-		pmtid_ev = np.extract(condition_2, sub_hit_pmtid) 			#Indices for which elements are True in eventID then extract the hit_pmtID
-		time_res_ev = np.extract(condition_2, sub_hit_residual)
-		pmttime_ev = np.extract(condition_2, sub_hit_pmttime)
-		hit_type_ev = np.extract(condition_2, sub_hit_type)
+		condition_ev = (mcID == i_id)
+		pmtid_ev = np.extract(condition_ev, hit_pmtid) 			#Indices for which elements are True in eventID then extract the hit_pmtID
+		time_res_ev = np.extract(condition_ev, hit_residual)
+		pmttime_ev = np.extract(condition_ev, hit_pmttime)
+		hit_type_ev = np.extract(condition_ev, hit_type)
 
 		#Positions and radi
 		mc_position_ev = []
@@ -123,9 +117,10 @@ def Analysis_simulation(file, evID):
 		mc_radius_ev = np.array([])
 		recons_radius_ev = np.array([])
 
-		for i in where(condition_2)[0]:
-			mc_position_ev.append(sub_mc_position[i])
-			recons_position_ev.append(sub_position[i])
+		for i in where(condition_ev)[0]:
+			mc_position_ev.append(mc_position[i])
+			recons_position_ev.append(position[i])
+
 		mc_position_ev = np.array(mc_position_ev)
 		recons_position_ev = np.array(recons_position_ev)
 
@@ -134,6 +129,7 @@ def Analysis_simulation(file, evID):
 			RECONS_radius = magnitude(recons_position_ev[i])
 			mc_radius_ev = np.append(mc_radius_ev, MC_radius)
 			recons_radius_ev = np.append(recons_radius_ev, RECONS_radius)
+		
 
 		#APPEND
 		recons_rad_ID = np.append(recons_rad_ID, recons_radius_ev[0])
@@ -149,52 +145,105 @@ def Analysis_simulation(file, evID):
 
 		#PMT type selection
 		valid_type = 1
-		valid_pmt_id = np.array([],dtype = np.int64)	#valid PMTid
+		valid_pmt_id = []													#valid PMTid
 
 		valid_info_pmt_i = where(pmt_type == valid_type)[0]
 
 		for i in valid_info_pmt_i:
-			valid_pmt_id = np.append(valid_pmt_id, pmt_id[i])
+			valid_pmt_id.append(pmt_id[i])
 
 		    
 		# Buscar información valida dentro del eventos:
-		#indices de info. valida
-		valid_info_ev_i = where(np.in1d(pmtid_ev, valid_pmt_id))
+		# indices de info. valida
+		valid_id_info_ev_i = where(np.in1d(pmtid_ev, valid_pmt_id))[0]
 
 		#información que deseo observar
-		time_res_ev_valid = np.array([])
-		pmt_id_ev_valid = np.array([], dtype = np.int64)
-		hit_type_valid = np.array([], dtype = np.int64)
+		valid_time_res_ev = []
+		valid_pmt_id_ev =[]
+		valid_hit_type_ev = []
+		valid_xyz_pmt_ev = []
+		valid_sph_pmt_ev = []
 
-		for j in valid_info_ev_i:
-			time_res_ev_valid = np.append(time_res_ev_valid, time_res_ev[j])
-			pmt_id_ev_valid = np.append(pmt_id_ev_valid, pmtid_ev[j])
-			hit_type_valid = np.append(hit_type_valid, hit_type_ev[j])
+		for j in valid_id_info_ev_i:
+			valid_time_res_ev.append(time_res_ev[j])
+			valid_pmt_id_ev.append(pmtid_ev[j])
+			valid_hit_type_ev.append(hit_type_ev[j])
 
-		#APPEND
-		hit_pmtid_ID.append(pmt_id_ev_valid)
-		hit_type_ID.append(hit_type_valid)
+		for k in valid_pmt_id_ev:
+			valid_xyz_pmt_ev.append(pmt_pos_xyz[k])
+			valid_sph_pmt_ev.append(pmt_pos_sph[k])
+
+
+		#Separate hit types:
+
+		index_hit_1 = where(np.in1d(valid_hit_type_ev, [1]))[0]
+		index_hit_2 = where(np.in1d(valid_hit_type_ev, [2]))[0]
+
+		pmt_xyz_hit_1_ev = []
+		pmt_sph_hit_1_ev = []
+
+		pmt_xyz_hit_2_ev = []
+		pmt_sph_hit_2_ev = []
+
+		time_residual_hit1_ev = []
+		time_residual_hit2_ev = []
+
+		hitpmt_ID_hit_1_ev = []
+		hitpmt_ID_hit_2_ev = []
+
+
+		for (i,j) in zip(index_hit_1, index_hit_2):
+
+		    pmt_xyz_hit_1_ev.append(valid_xyz_pmt_ev[i])
+		    pmt_sph_hit_1_ev.append(valid_sph_pmt_ev[i])
+		    time_residual_hit1_ev.append(valid_time_res_ev[i])
+		    hitpmt_ID_hit_1_ev.append(valid_pmt_id_ev[i])
+
+		    pmt_xyz_hit_2_ev.append(valid_xyz_pmt_ev[j])
+		    pmt_sph_hit_2_ev.append(valid_sph_pmt_ev[j])
+		    time_residual_hit2_ev.append(valid_time_res_ev[j])
+		    hitpmt_ID_hit_2_ev.append(valid_pmt_id_ev[j])
+
+		#APPEND-------------------------------------------------------------------------------------------
+		hit_pmtid_ID.append(valid_pmt_id_ev)
+		hit_type_ID.append(valid_hit_type_ev)
 		mc_postion_vertex.append(mc_position_ev[0])
+		time_res_ID.append(valid_time_res_ev)
+		pmt_xyz_ID.append(valid_xyz_pmt_ev)
+		pmt_sph_ID.append(valid_sph_pmt_ev)
 
-		#Save useful plots
-		#matplotlib.use('Agg') #-----------> Dont show in screen Plots
-		#hist(time_res_ev_valid, 100, 'time_res_ev', title = 'ID=' +str(i_id))
+		pmt_xyz_hit_1_ID.append(pmt_xyz_hit_1_ev)
+		pmt_sph_hit_1_ID.append(pmt_sph_hit_1_ev)
+		time_residual_hit1_ID.append(time_residual_hit1_ev)
+		hitpmt_hit_1_ID.append(hitpmt_ID_hit_1_ev)
 
-	#Construct DataFrame
+		pmt_xyz_hit_2_ID.append(pmt_xyz_hit_2_ev)
+		pmt_sph_hit_2_ID.append(pmt_sph_hit_2_ev)
+		time_residual_hit2_ID.append(time_residual_hit2_ev)
+		hitpmt_hit_2_ID.append(hitpmt_ID_hit_2_ev)
 
-	#AGREGAR COORDENADAS DE GENERACIÓN DEL EVENTO
+
 	data = {'eventID': evID,
-			'hitpmt ID': hit_pmtid_ID,
-			'hit type': hit_type_ID,
-			'mc coordinates': mc_postion_vertex,
-			'mc radius': mc_rad_ID,
-			'reconst radius': recons_rad_ID,
-			'reconst error': reconstructed_error_ID,
-			}
+		'hitpmt ID': hit_pmtid_ID,
+		'hit type': hit_type_ID,
+		'time residual': time_res_ID,
+		'mc coordinates': mc_postion_vertex,
+		'mc radius': mc_rad_ID, #P
+		'reconst radius': recons_rad_ID, #P
+		'reconst error': reconstructed_error_ID, #P
+		'PMT xyz': pmt_xyz_ID,
+		'PMT spherical': pmt_sph_ID,
+		'hitpmt ID hit 1': hitpmt_hit_1_ID,
+		'hitpmt ID hit 2': hitpmt_hit_2_ID,
+		'time residual hit 1': time_residual_hit1_ID,
+		'time residual hit 2': time_residual_hit2_ID,
+		'xyz hit 1': pmt_xyz_hit_1_ID,
+		'spherical hit 1': pmt_sph_hit_1_ID,
+		'xyz hit 2': pmt_xyz_hit_2_ID,
+		'spherical hit 2': pmt_sph_hit_2_ID,
+		}
 
 	DataFrame_analysis = pd.DataFrame(data)
 
 		
 	return DataFrame_analysis
-
-# cuantos más motivos mejor :)
