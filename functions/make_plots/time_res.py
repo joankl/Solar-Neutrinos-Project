00f11d412a1f8a322d21ca227f55ef963667b076
joankl <joankphysic@gmail.com>
2026-05-09 13:44:40 +0100

    updating analysis

--- functions/make_plots/time_res.py
+++ functions/make_plots/time_res.py
@@ -1,31 +1,36 @@
 '''
 Plot maker of the Time residual distribution.
 Be careful chosing the t_res_min and t_res_max to construct the binedges
-of the time residual distribution
+of the time residual distribution in the sense that we must respect the 
+cuts implements in time residual when constructing the numpy arrays.
 
 Creation: 05/05/2026
 '''
 
 import numpy as np
+
 import glob
 import os
+
 import matplotlib.pyplot as plt
+from matplotlib.ticker import MultipleLocator, ScalarFormatter
+from matplotlib import font_manager
 
 
 print('Reading Data ...')
 
-read_dir = '/lstore/sno/joankl/solar_analysis/real_data/bisMSB/Analysis*/ratDS_output/np_files/'
+read_dir = '/lstore/sno/joankl/solar_analysis/mc_data/main_simulations/2p2_ppo/solar_8BNue/ratds_output/np_files/'
 read_dir_list = glob.glob(read_dir)
-save_dir = '/lstore/sno/joankl/solar_analysis/real_data/bisMSB/plots/figures/'
+save_dir = '/lstore/sno/joankl/solar_analysis/mc_data/main_simulations/2p2_ppo/solar_8BNue/ratds_output/plots/'
 
 os.makedirs(save_dir, exist_ok=True)
 
-bins = 80
-E_cut_list = [5, 6, 8, 10]
-R_cut_list = [5500, 4500, 3500]
+bins = 150
+E_cut_list = [5]
+R_cut_list = [5500]
 
-t_res_min_cut = -150
-t_res_max_cut = 250
+t_res_min_cut = -5
+t_res_max_cut = 5
 
 # 1 - Prepare the structure to save the counts of the histograms.
 bin_edges = np.linspace(t_res_min_cut, t_res_max_cut, bins + 1)
@@ -64,31 +69,48 @@ for read_dir_i in read_dir_list:
 
 print('Data fully processed. Generating Plots...')
 
-# =========== Plot Construction ===========
 print(f'time residual limits defined as: [{t_res_min_cut},{t_res_max_cut}] ns')
 print(f'time residual limits found in histogram as: [{min(hist_bin_edges):.0f},{max(hist_bin_edges):.0f}] ns')
 
+# =========== Plot Construction ===========
+
+font_style_title = {'family':'serif', 'weight': 'normal','color':'black','size':13}
+font_style_axis= {'family':'serif', 'weight': 'normal','color':'black','size':12}
+font_prop = font_manager.FontProperties(family=font_style_axis['family'], weight=font_style_axis['weight'], size=font_style_axis['size'])
+
 for Ecut_i in E_cut_list:
     
-	fig, axes = plt.subplots(1, len(R_cut_list), figsize=(24, 5.8))
+	fig, axes = plt.subplots(1, len(R_cut_list), figsize=(8, 6))
 
 	for i_dx, Rcut_i in enumerate(R_cut_list):
 		ax = axes[i_dx]
 
-		# Recuperamos los conteos que calculamos en el loop de lectura
 		counts = hist_data[Ecut_i][Rcut_i]
 
-		ax.hist(bin_edges[:-1], bins=bin_edges, weights=counts, histtype='step', color='blue', linewidth=1.5)
+		ax.hist(bin_edges[:-1], bins=bin_edges, weights=counts, density=True, histtype='step', color='blue', linewidth=1.5)
 
-		ax.set_title(rf'E $\geq$ {Ecut_i} (MeV) & R $\leq$ {Rcut_i*10**-3:.1f} (m)')
 		ax.set_yscale('log')
-		ax.set_xlabel(r'$t_{res}$')
+		ax.set_xlabel(r'$t_{res}$', fontdict = font_style_axis)
+		ax.ylabel(r'Prob. Density', fontdict = font_style_axis)
 		ax.set_xlim(t_res_min_cut, t_res_max_cut)
 
-	# Titulo General
-	plt.suptitle(rf'Time Residual Distribution - BisMSB $^8$B-$\nu_e$ Candidates - $t_{{res}}$: [{t_res_min_cut:.0f}, {t_res_max_cut:.0f}] (ns)', fontsize=13, y=1.02)
+		# --- Markers ---
+		ax.xaxis.set_minor_locator(MultipleLocator(0.5))
+		ax.xaxis.set_major_locator(MultipleLocator(1))
 
-	save_path = save_dir + f'time_res_E_{Ecut_i}_MeV_All_Rcuts.png'
+		#ax.yaxis.set_minor_locator(MultipleLocator(0.01))
+		#ax.yaxis.set_major_locator(MultipleLocator(0.05))
+
+		ax.tick_params(which='minor', top=True, bottom=True, left=True, right=True)
+		ax.tick_params(which='major', top=True, bottom=True, left=True, right=True)
+
+		ax.set_title(rf'Time Residual Distribution - 2.2PPO $^8$B-$\nu_e$ MC - E $\geq$ {Ecut_i} (MeV) & R $\leq$ {Rcut_i*10**-3:.1f} (m)', fontdict = font_style_title)
+
+	# Titulo General
+	#plt.suptitle(rf'Time Residual Distribution - BisMSB $^8$B-$\nu_e$ Candidates - $t_{{res}}$: [{t_res_min_cut:.0f}, {t_res_max_cut:.0f}] (ns)', fontsize=13, y=1.02)
+
+	#save_path = save_dir + f'time_res_E_{Ecut_i}_MeV_All_Rcuts.png'
+	save_path = save_dir + f'time_res_E_{Ecut_i}_MeV_R_{Rcut_i}_mm.png'
 	plt.savefig(save_path, dpi=300, bbox_inches='tight')
 
 	# Limpiamos la figura para que no se superpongan en la RAM
